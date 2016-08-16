64a69b454d14ad79fb2cfed99b028addfc40bc0b
hongbiliang <hongbiliang@star-net.cn>
2016-08-16 16:28:26 +0800

    添加数据库帮助类，以及各表对应的实体类

---
+++ app/src/main/java/com/starnet/root/coolweather/model/City.java
@@ -0,0 +1,43 @@
+package com.starnet.root.coolweather.model;
+
+/**
+ * Created by root on 16-8-16.
+ */
+public class City {
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
